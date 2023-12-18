d0669dfebe81ab296a3d7e955755afe3b97d6573
Toma-Bad <toma.badescu@gmail.com>
2023-12-18 23:19:25 +0100

    first

--- ecs_entity.py
+++ ecs_entity.py
@@ -1,19 +1,38 @@
 class BasicProps:
-    def __init__(self,hp,str,dex):
+    def __init__(self,hp=100,strength=10,dexterity=10):
         self.hp = hp
-        self.str = str
-        self.dex = dex
+        self.strength = strength
+        self.dexterity = dexterity
 
+class Modifiers:
+    def __init__(self,**modifiers):
+        self.__dict__.update(modifiers)
 class BasicRace:
-    def __init__(self,racetype,modifiers):
+    def __init__(self,racetype,**modifiers):
         self.racetype = racetype
-        self.modifiers = modifiers
+        self.modifiers = Modifiers(**modifiers)
+
+class Orc(BasicRace):
+    def __init__(self):
+        super().__init__("orcish",hp=2,strength=5)
+
 
 class Entities:
     ids = dict()
     components = dict()
     positions = dict()
-    def __init__(self,pos,*comps):
+    def __init__(self,name,pos,*component_args):
+        self.name = name
         self.pos = pos
-        self.comp_list
+        self.id = id(self)
+        t_comp_dict = {type(_c).__name__: _c for _c in component_args}
+        self.__dict__.update(t_comp_dict)
+        Entities.ids[self.id] = self
+        if pos in Entities.positions:
+            Entities.positions[pos].append(self.id)
+        else:
+            Entities.positions[pos] = self.id
 
+if __name__ == "__main__":
+    ee1 = Entities("bob",(1,1),BasicProps(),Orc())
+    ee2 = Entities("jim",(2,1),BasicProps(strength=12),Orc())
