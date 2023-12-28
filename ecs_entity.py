02af0ae24a46dfe2174f26b64ac793ed01bef02d
Toma-Bad <toma.badescu@gmail.com>
2023-12-28 16:32:51 +0100

    created component baseclass and some components, inventory needs work

--- ecs_entity.py
+++ ecs_entity.py
@@ -1,5 +1,17 @@
 import json
 from types import SimpleNamespace
+import numpy as np
+class BaseObject:
+    def __init__(self, *components):
+        self.id = id(self)
+        self.component = SimpleNamespace(**{type(_c).__name__:_c for _c in components})
+    def add_component(self, *components):
+        self.component.__dict__.update({type(_c).__name__:_c for _c in components})
+    def remove_component(self, component_name):
+        try:
+            self.component.__delattr__(component_name)
+        except Exception as e:
+            print(e)
 
 
 class Scene:
@@ -52,26 +64,64 @@ class Scene:
                     elif base_object.id in self.obj_components[_c]:
                         self.obj_components[_c].remove(base_object.id)
 
+    def rm_comp_from_obj(self,base_object : BaseObject,component_name : str):
+        if component_name in base_object.component.__dict__:
+            base_object.remove_component(component_name)
+            if component_name in self.obj_components:
+                if self.obj_components[component_name] == {base_object.id}:
+                    del self.obj_components[component_name]
+                elif base_object.id in self.obj_components[component_name]:
+                    self.obj_components[component_name].remove(base_object.id)
+    def add_comp_to_obj(self,base_object : BaseObject,component_instance):
+        base_object.add_component(component_instance)
+        component_name = type(component_instance).__name__
+        if component_name in self.obj_components:
+            self.obj_components[component_name].add(base_object.id)
+        else:
+            self.obj_components[component_name] = {base_object.id}
 
-class BaseObject:
-    def __init__(self, *components):
-        self.id = id(self)
-        self.component = SimpleNamespace(**{type(_c).__name__:_c for _c in components})
-    def add_component(self, *components):
-        self.component.__dict__.update({type(_c).__name__:_c for _c in components})
-    def remove_component(self, component_name):
-        try:
-            self.component.__delattr__(component_name)
-        except Exception as e:
-            print(e)
 
 
-class BasicProps:
+class BaseComponent:
+    def __init__(self,**kwargs):
+        self.__dict__.update(kwargs)
+    @classmethod
+    def load_from_file(cls, filename):
+        with open(filename) as fin:
+            data_dict = json.load(fin)
+        return cls(**data_dict)
+
+class BasicProps(BaseComponent):
     def __init__(self, **basic_properties):
         self.__dict__.update(basic_properties)
-    def load_from_file(self, filename):
-        with open(filename) as fin:
-            self.__dict__.update(json.load(fin))
+
+class Inventory(BaseComponent):
+    #to do: add str representation for the items in dict
+    def __init__(self,**items):
+        self.storage = dict()
+        self.storage.update(items)
+        self.equipped = dict()
+        self.weight = sum([_i.weight for _i in self.storage.values()])
+    def set_equip_from_storage(self,item_name):
+        if item_name in self.storage and item_name not in self.equipped:
+            self.equipped[item_name] = self.storage[item_name]
+    def set_unequip(self,item_name):
+        if item_name in self.storage and item_name in self.equipped:
+            del self.equipped[item_name]
+    def add_items(self,**items):
+        self.storage.update(items)
+        self.weight += sum([_i.weight for _i in items.values()])
+    def remove_items(self,equipped = False,**items):
+        for _it_name in items:
+            if _it_name in self.storage:
+                del self.storage[_it_name]
+            if equipped:
+                if _it_name in self.equipped:
+                    del self.equipped[_it_name]
+        self.weight -= sum([_i.weight for _i in items.values()])
+
+
+
 class Modifiers:
     def __init__(self, **modifiers):
         self.__dict__.update(modifiers)
